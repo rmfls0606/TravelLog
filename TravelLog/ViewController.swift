8ff6d92862b57c4f67d641dba36fa7ea32f0cdf4
rmfls0606 <rmfls0606@naver.com>
2025-09-29 19:44:53 +0900

    Initial Commit

---
+++ TravelLog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelLog
+//
+//  Created by 이상민 on 9/29/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
