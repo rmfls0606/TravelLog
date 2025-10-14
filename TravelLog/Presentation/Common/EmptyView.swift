84f4f2cb422d0c0f18dfa801a49aa4539053f2ad
rmfls0606 <rmfls0606@naver.com>
2025-10-14 17:51:44 +0900

    style: EmptyView 아이콘 컨테이너 추가 및 레이아웃 개선

--- TravelLog/Presentation/Common/EmptyView.swift
+++ TravelLog/Presentation/Common/EmptyView.swift
@@ -11,14 +11,43 @@ import SnapKit
 final class EmptyView: BaseView {
     
     // MARK: - UI
-    private let iconView = UIImageView()
-    private let titleLabel = UILabel()
-    private let subtitleLabel = UILabel()
+    private let iconContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .systemBlue.withAlphaComponent(0.15)
+        view.layer.cornerRadius = 20
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private let iconView: UIImageView = {
+        let view = UIImageView()
+        view.tintColor = .systemBlue
+        view.contentMode = .scaleAspectFit
+        return view
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .boldSystemFont(ofSize: 16)
+        label.textColor = .darkGray
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let subtitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .darkGray
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        return label
+    }()
+    
     private let stackView: UIStackView = {
         let view = UIStackView()
         view.axis = .vertical
         view.alignment = .center
-        view.spacing = 12
+        view.spacing = 8
         return view
     }()
     
@@ -33,31 +62,24 @@ final class EmptyView: BaseView {
         // setup
         if let iconName {
             iconView.image = UIImage(systemName: iconName)
-            iconView.tintColor = .systemGray3
-            iconView.contentMode = .scaleAspectFit
         } else {
             iconView.isHidden = true
+            iconContainerView.isHidden = true
         }
         
         titleLabel.text = title
-        titleLabel.font = .boldSystemFont(ofSize: 16)
-        titleLabel.textColor = .darkGray
-        titleLabel.textAlignment = .center
         
         if let subtitle {
             subtitleLabel.text = subtitle
-            subtitleLabel.font = .systemFont(ofSize: 14)
-            subtitleLabel.textColor = .gray
-            subtitleLabel.numberOfLines = 0
-            subtitleLabel.textAlignment = .center
         } else {
             subtitleLabel.isHidden = true
         }
     }
     
     override func configureHierarchy() {
+        iconContainerView.addSubview(iconView)
         addSubview(stackView)
-        [iconView, titleLabel, subtitleLabel].forEach { stackView.addArrangedSubview($0) }
+        [iconContainerView, titleLabel, subtitleLabel].forEach { stackView.addArrangedSubview($0) }
     }
     
     override func configureLayout() {
@@ -67,7 +89,12 @@ final class EmptyView: BaseView {
         }
         
         iconView.snp.makeConstraints { make in
-            make.size.equalTo(60)
+            make.center.equalToSuperview()
+            make.size.equalTo(24)
+        }
+        
+        iconContainerView.snp.makeConstraints { make in
+            make.size.equalTo(40)
         }
     }
     
