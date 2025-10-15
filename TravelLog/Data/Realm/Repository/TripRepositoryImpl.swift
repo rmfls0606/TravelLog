77c5a7753c9054f85ee638d746ffc030e0f00a34
rmfls0606 <rmfls0606@naver.com>
2025-10-15 23:44:04 +0900

    feat: TripRepository가 TripRealmDataSource를 의존성으로 받아 실제 데이터 접근을 담당하도록 구현

---
+++ TravelLog/Data/Realm/Repository/TripRepositoryImpl.swift
@@ -0,0 +1,32 @@
+//
+//  TripRepositoryImpl.swift
+//  TravelLog
+//
+//  Created by 이상민 on 10/15/25.
+//
+
+import Foundation
+import RxSwift
+
+final class TripRepositoryImpl: TripRepository{
+    private let dataSource: TripRealmDataSource
+    
+    init(dataSource: TripRealmDataSource = TripRealmDataSource()) {
+        self.dataSource = dataSource
+    }
+    
+    func createTrip(
+        departure: CityTable,
+        destination: CityTable,
+        startDate: Date,
+        endDate: Date,
+        transport: Transport
+    ) -> Completable {
+        return dataSource.createTrip(
+            departure: departure,
+            destination: destination,
+            startDate: startDate,
+            endDate: endDate,
+            transport: transport)
+    }
+}
