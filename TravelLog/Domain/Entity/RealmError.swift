6537d6d2f8bb06088b7b4b4480867a021fec538a
rmfls0606 <rmfls0606@naver.com>
2025-10-15 21:58:36 +0900

    feat: RealmError 엔티티 추가 및 도메인 공통 에러 처리

---
+++ TravelLog/Domain/Entity/RealmError.swift
@@ -0,0 +1,25 @@
+//
+//  RealmError.swift
+//  TravelLog
+//
+//  Created by 이상민 on 10/15/25.
+//
+
+import Foundation
+
+enum RealmError: LocalizedError{
+    case saveFailure
+    case fetchFailure
+    case instanceNotFound
+    
+    var errorDescription: String?{
+        switch self {
+        case .saveFailure:
+            return "저장이 완료되지 않았습니다.\n잠시 후 다시 시도해주세요."
+        case .fetchFailure:
+            return "데이터를 불러올 수 없습니다.\n잠시 후 다시 시도해주세요."
+        case .instanceNotFound:
+            return "데이터 처리 중 문제가 발생했습니다.\n잠시 후 다시 시도해주세요."
+        }
+    }
+}
