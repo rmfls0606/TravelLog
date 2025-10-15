334be121fd391547a1f2c76f165f62a05c60498b
rmfls0606 <rmfls0606@naver.com>
2025-10-16 03:16:59 +0900

    feat: RealmError에 삭제 실패 케이스 추가

--- TravelLog/Domain/Entity/RealmError.swift
+++ TravelLog/Domain/Entity/RealmError.swift
@@ -10,6 +10,7 @@ import Foundation
 enum RealmError: LocalizedError{
     case saveFailure
     case fetchFailure
+    case deleteFailure
     case instanceNotFound
     
     var errorDescription: String?{
@@ -18,6 +19,8 @@ enum RealmError: LocalizedError{
             return "저장이 완료되지 않았습니다.\n잠시 후 다시 시도해주세요."
         case .fetchFailure:
             return "데이터를 불러올 수 없습니다.\n잠시 후 다시 시도해주세요."
+        case .deleteFailure:
+            return "데이터를 삭제하는데 실패했습니다.\n잠시 후 다시 시도해주세요."
         case .instanceNotFound:
             return "데이터 처리 중 문제가 발생했습니다.\n잠시 후 다시 시도해주세요."
         }
