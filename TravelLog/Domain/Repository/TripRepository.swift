972a94f9fe9a7fa57a88d1ca4d3e381986ac28fb
rmfls0606 <rmfls0606@naver.com>
2025-10-15 23:42:45 +0900

    feat: UseCase가 DataSource에 직접 접근하지 않도록 TripRepository 추상화 계층 추가

---
+++ TravelLog/Domain/Repository/TripRepository.swift
@@ -0,0 +1,19 @@
+//
+//  TripRepository.swift
+//  TravelLog
+//
+//  Created by 이상민 on 10/15/25.
+//
+
+import Foundation
+import RxSwift
+
+protocol TripRepository{
+    func createTrip(
+        departure: CityTable,
+        destination: CityTable,
+        startDate: Date,
+        endDate: Date,
+        transport: Transport
+    ) -> Completable
+}
