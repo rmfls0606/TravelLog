5b9995febba08cbda6c5a55eb22147562c021d20
rmfls0606 <rmfls0606@naver.com>
2025-10-19 16:40:22 +0900

    feat: GradientStyle에 새로운 컬러 case 추가

--- TravelLog/Presentation/Common/GradientStyle.swift
+++ TravelLog/Presentation/Common/GradientStyle.swift
@@ -10,6 +10,7 @@ import UIKit
 //앱 전체에서 재사용 가능한 그라데이션 스타일 정의
 enum GradientStyle{
     case bluePurple
+    case softBluePurple
     case custom([UIColor])
     
     var colors: [UIColor]{
@@ -19,6 +20,12 @@ enum GradientStyle{
                 UIColor(red: 90/255, green: 140/255, blue: 255/255, alpha: 1),
                 UIColor(red: 130/255, green: 80/255, blue: 255/255, alpha: 1)
             ]
+        case .softBluePurple:
+            return [
+                UIColor(red: 220/255, green: 230/255, blue: 255/255, alpha: 0.45), // 연한 하늘색
+                UIColor(red: 235/255, green: 220/255, blue: 255/255, alpha: 0.45)
+            ]
+            
         case .custom(let colors):
             return colors
         }
