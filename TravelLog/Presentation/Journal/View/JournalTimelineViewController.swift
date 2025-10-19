a1bd9ef7ef9ec45d7b0560c478a7c8b1a740e682
rmfls0606 <rmfls0606@naver.com>
2025-10-19 18:46:11 +0900

    feat: 추가 화면 중첩 이동 문제 수정

--- TravelLog/Presentation/Journal/View/JournalTimelineViewController.swift
+++ TravelLog/Presentation/Journal/View/JournalTimelineViewController.swift
@@ -41,16 +41,7 @@ final class JournalTimelineViewController: BaseViewController {
         fetchTrip(tripId)
     }
     required init?(coder: NSCoder) { fatalError("init(coder:) has not been implemented") }
-    
-    // MARK: - Lifecycle
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        configureHierarchy()
-        configureLayout()
-        configureView()
-        configureBind()
-    }
-    
+        
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         
