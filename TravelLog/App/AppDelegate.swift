4eca3d214a6f15926123b3bb2f27494ff00fe511
rmfls0606 <rmfls0606@naver.com>
2025-10-20 14:07:29 +0900

    style: 네비게이션 back title 숨김 처리

--- TravelLog/App/AppDelegate.swift
+++ TravelLog/App/AppDelegate.swift
@@ -14,6 +14,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backButtonAppearance.normal.titlePositionAdjustment = UIOffset(horizontal: -1000, vertical: 0)
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
         return true
     }
 
