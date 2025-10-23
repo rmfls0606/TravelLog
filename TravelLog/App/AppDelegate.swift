8e4bd4572fdd0d0c8ec2ca095af20b7f23afaf58
rmfls0606 <rmfls0606@naver.com>
2025-10-24 03:34:03 +0900

    chore: 마이그레이션 설정 추가(Version 1)

--- TravelLog/App/AppDelegate.swift
+++ TravelLog/App/AppDelegate.swift
@@ -6,14 +6,18 @@
 //
 
 import UIKit
+import RealmSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
+    
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        migration()
+        
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backButtonAppearance.normal.titlePositionAdjustment = UIOffset(horizontal: -1000, vertical: 0)
@@ -21,21 +25,34 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         UINavigationBar.appearance().scrollEdgeAppearance = appearance
         return true
     }
-
+    
     // MARK: UISceneSession Lifecycle
-
+    
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    private func migration(){
+        
+        let config = Realm.Configuration(schemaVersion: 1) { migration, oldSchemaVersion in
+            //JournalBlockTable에 링크 미리보기를 위한 linkTitle, linkDescription, linkImagePath 컬럼 추가
+            if oldSchemaVersion < 1 {}
+        }
+        
+        Realm.Configuration.defaultConfiguration = config
+        do {
+            _ = try Realm()
+        } catch {
+            print("Realm Migration 실패:", error.localizedDescription)
+        }
+    }
 }
 
