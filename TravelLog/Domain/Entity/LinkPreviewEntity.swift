9163750c5b655fc22c3140c8d1b953d99e92cb67
rmfls0606 <rmfls0606@naver.com>
2025-10-24 19:30:47 +0900

    feat: 링크 미리보기 처리를 위한 Domain DTO 추가

---
+++ TravelLog/Domain/Entity/LinkPreviewEntity.swift
@@ -0,0 +1,15 @@
+//
+//  LinkPreviewEntity.swift
+//  TravelLog
+//
+//  Created by 이상민 on 10/24/25.
+//
+
+import UIKit
+
+struct LinkPreviewEntity {
+    let url: String
+    let title: String?
+    let description: String?
+    let imageFilename: String?
+}
