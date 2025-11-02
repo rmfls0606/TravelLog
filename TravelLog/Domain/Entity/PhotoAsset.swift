ec387810431885e0bde8ea267bd9d3f59af0d28c
rmfls0606 <rmfls0606@naver.com>
2025-11-03 01:26:57 +0900

    feat: PhotoAsset 모델 추가

---
+++ TravelLog/Domain/Entity/PhotoAsset.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoAsset.swift
+//  TravelLog
+//
+//  Created by 이상민 on 11/2/25.
+//
+
+import Photos
+
+struct PhotoAsset: Hashable{
+    let identifier: String
+    let asset: PHAsset
+}
