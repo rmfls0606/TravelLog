f439ed07899e85c1c5343bde4403ca21a4dc0172
rmfls0606 <rmfls0606@naver.com>
2025-11-03 01:26:15 +0900

    feat: PhotoPickerViewModel 추가 및 권한 체크 로직 구현

---
+++ TravelLog/Presentation/Journal/ViewModel/PhotoPickerViewModel.swift
@@ -0,0 +1,61 @@
+//
+//  PhotoPickerViewModel.swift
+//  TravelLog
+//
+//  Created by 이상민 on 11/2/25.
+//
+
+import Foundation
+import PhotosUI
+import UIKit
+
+final class PhotoPickerViewModel{
+    
+    private let observer = PhotoLibraryObserver()
+    private(set) var allAssets: [PHAsset] = []
+    
+    // 뷰컨에 보낼 콜백
+    var onAssetsChanged: (([PHAsset]) -> Void)?
+    var onPermissionDenied: (() -> Void)?
+    
+    init() {
+        // 옵저버에서 변화 감지
+        observer.changeHandler = { [weak self] updated in
+            self?.allAssets = updated
+            self?.onAssetsChanged?(updated)
+        }
+    }
+    
+    // MARK: - 권한 확인
+    func checkPermission() async {
+        let status = PHPhotoLibrary.authorizationStatus(for: .readWrite)
+        switch status {
+        case .notDetermined:
+            let newStatus = await PHPhotoLibrary.requestAuthorization(for: .readWrite)
+            if newStatus == .authorized || newStatus == .limited {
+                observer.fetchAssets()
+            } else {
+                self.onPermissionDenied?()
+            }
+        case .authorized, .limited:
+            observer.fetchAssets()
+        default:
+            self.onPermissionDenied?()
+        }
+    }
+    
+    // MARK: - 썸네일
+    func requestThumbnail(for asset: PHAsset, targetSize: CGSize) async -> UIImage? {
+        await withCheckedContinuation { continuation in
+            let manager = PHCachingImageManager.default()
+            let options = PHImageRequestOptions()
+            options.isNetworkAccessAllowed = true
+            options.resizeMode = .fast
+            options.deliveryMode = .opportunistic
+            
+            manager.requestImage(for: asset, targetSize: targetSize, contentMode: .aspectFill, options: options) { image, _ in
+                continuation.resume(returning: image)
+            }
+        }
+    }
+}
