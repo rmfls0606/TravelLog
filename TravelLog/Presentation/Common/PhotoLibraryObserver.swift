b22e715eca253ae92404ff6f5b47576ae30e8a68
rmfls0606 <rmfls0606@naver.com>
2025-11-04 02:51:31 +0900

    feat: PhotoLibraryObserver 변경 감지 및 fetchResult 전달 구조 개선

--- TravelLog/Presentation/Common/PhotoLibraryObserver.swift
+++ TravelLog/Presentation/Common/PhotoLibraryObserver.swift
@@ -10,7 +10,7 @@ import Photos
 final class PhotoLibraryObserver: NSObject, PHPhotoLibraryChangeObserver {
 
     private var fetchResult: PHFetchResult<PHAsset>?
-    var changeHandler: (([PHAsset]) -> Void)?
+    var changeHandler: ((PHFetchResult<PHAsset>) -> Void)?
 
     override init() {
         super.init()
@@ -24,25 +24,19 @@ final class PhotoLibraryObserver: NSObject, PHPhotoLibraryChangeObserver {
     func fetchAssets(){
         let options = PHFetchOptions()
         options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
-        fetchResult = PHAsset.fetchAssets(with: .image, options: options)
-        updateAssets()
-    }
-    
-    func updateAssets(){
-        guard let fetchResult = fetchResult else { return }
-        //앱에서 사용할 수 있는 형태 변환
-        let assets = fetchResult.objects(at: IndexSet(0..<fetchResult.count))
-        self.changeHandler?(assets)
+        let result = PHAsset.fetchAssets(with: .image, options: options)
+        self.fetchResult = result
+        changeHandler?(result)
     }
 
     // 앨범 변경 감지
     func photoLibraryDidChange(_ changeInstance: PHChange) {
         guard let fetchResult,
               let changes = changeInstance.changeDetails(for: fetchResult) else { return }
-
-        DispatchQueue.main.async {
-            self.fetchResult = changes.fetchResultAfterChanges
-            self.updateAssets()
+        let newResult = changes.fetchResultAfterChanges
+        self.fetchResult = newResult
+        DispatchQueue.main.async { [weak self] in
+            self?.changeHandler?(newResult)
         }
     }
 }
