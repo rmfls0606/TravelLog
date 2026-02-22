bfb29052a7776d9c25687f2c2ea69a099b09d029
rmfls0606 <rmfls0606@naver.com>
2026-02-22 22:21:36 +0900

    feat: 키패드에 따른 화면 조절 로직 추가

--- TravelLog/App/AppDelegate.swift
+++ TravelLog/App/AppDelegate.swift
@@ -8,17 +8,18 @@
 import UIKit
 import RealmSwift
 import Firebase
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
-    
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FirebaseApp.configure()
         
         migration()
+        
+        IQKeyboardManager.shared.isEnabled = true
         
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
